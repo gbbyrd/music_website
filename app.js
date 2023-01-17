b5b26c67811777a072dfc446694937defba9d1cd
gbbyrd <graysonbyrd4@gmail.com>
2023-01-17 00:14:45 -0500

    Added Spotify authorization request functionality

--- app.js
+++ app.js
@@ -1,4 +1,60 @@
-const express = require('express');
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = '71e7b9bde49745bfa1fd4ffeae3a9c37'; // Your client id
+var client_secret = 'dbc7470cc2c440d38162e305e37f7c7b'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+  
+var stateKey = 'spotify_auth_state';
 
 var app = express();
 
+app.use(express.static(__dirname + 'public'))
+   .use(cors())
+   .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope = 'user-read-private user-read-email';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+// add views folder for templates
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res){
+    res.render('home_page');
+})
+
+console.log('Listening at localhost:3000')
+app.listen(3000)
